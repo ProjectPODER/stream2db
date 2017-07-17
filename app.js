4223528c983c568401928c38e71b21ed4cfd387f
kevin <kevin@rindecuentas.org>
2017-07-17 14:50:19 -0500

    refactor   * add cmdline args   * split db and and args   * stream to es or mongo

--- app.js
+++ app.js
@@ -2,22 +2,21 @@ const request = require('request')
   , JSONStream = require('JSONStream')
   , mapValues = require('lodash.mapvalues')
   , keys = require('lodash.keys')
-  , assign = require('lodash.assign')
-  , elasticsearch = require('elasticsearch')
   , hash = require('object-hash')
-  , etl = require('etl')
-  , client = new elasticsearch.Client({
-      host: 'localhost:9200',
-    //  log: 'trace',
- });
+  , etl = require('etl');
 
-const INDEX = 'poder-compranet';
-// const PROXY_URL = 'http://localhost:9000';
-const PROXY_URL = 'https://excel2json.herokuapp.com';
-const timestamp = new Date().getTime();
-const SOURCES = process.argv.slice(2).map((e) => {
-  return `${PROXY_URL}/${e}`;
-});
+const args = require('./lib/args');
+const db = require('./lib/db');
+const SOURCES = args.sources;
+
+let cmd;
+
+if (args.db === 'mongo') {
+  const collection = db.get('compranet', {castIds: false});
+  cmd = etl.mongo.insert(collection)
+} else {
+  cmd = etl.elastic.index(db, args.db, 'compranet')
+}
 
 const KNOWN_BOOLS = [
   'CONVENIO_MODIFICATORIO',
@@ -26,64 +25,28 @@ const KNOWN_BOOLS = [
   'PLURIANUAL',
 ];
 
-const ping = client.ping({
-  requestTimeout: 30000,
-});
-
-const propertiesMap = {
-  IMPORTE_CONTRATO: { type : "float" },
-  CONVENIO_MODIFICATORIO: {type: 'boolean'},
-  CONTRATO_MARCO: {type: 'boolean'},
-  COMPRA_CONSOLIDADA: {type: 'boolean'},
-  PLURIANUAL: {type: 'boolean'},
-  FECHA_FIN: { type: 'date'},
-  FECHA_INICIO: { type: 'date'},
-  FECHA_APERTURA_PROPOSICIONES: { type: 'date'},
-  FECHA_CELEBRACION: { type: 'date'},
-  timestamp: { type : "date", format: "epoch_millis" },
-}
-
-function createIndex() {
-  // if exists return mapping
-  // else create
-  return client.indices.exists({
-    index: INDEX,
-  }).then((exists) => {
-    if (exists) {
-      return client.indices.getMapping({
-        index: INDEX,
-        type: 'compranet',
-      });
+function valueMap(data) {
+  return mapValues(data, (v,k,o) => {
+    if (/FECHA/.test(k)) {
+     return v.replace("GMT", "").trim();
     }
-    return client.indices.create({
-      index: 'poder-compranet',
-      body: {
-        mappings: {
-          compranet: {
-            properties: propertiesMap,
-          }
-        }
+    if (KNOWN_BOOLS.indexOf(k) > -1) {
+      if (v == 0) {
+        return false;
       }
-    });
+      if (v == 1) {
+        return true;
+      }
+    }
+    return v;
   });
 }
 
-function valueMap(data) {
-  return mapValues(data, (v,k,o) => {
-   if (KNOWN_BOOLS.indexOf(k) > -1) {
-     if (v == 0) {
-       return false
-     }
-     if (v == 1) {
-       return true
-     }
-   }
-   return v;
- });
-}
-
-const seen = [];
-function web2es(mapping, url) {
+const seenContractCodes = [];
+const seenHashes = [];
+let countAll = 0;
+let countIndexed = 0;
+function web2es(url) {
   request
     .get({ url })
     .on('error', (error) => {
@@ -91,18 +54,27 @@ function web2es(mapping, url) {
     })
     .pipe(JSONStream.parse())
     .pipe(etl.map(data => {
+      console.log(countIndexed);
+      countAll += 1;
       // check for document corruption
       if (data.hash !== hash(data.body)) {
         throw new Error('currupted document')
       }
       if (data.body.CODIGO_CONTRATO) {
+        countIndexed += 1;
         // throw away contracts w/ no code
-        if (seen.indexOf(data.body.CODIGO_CONTRATO) > -1) {
-          console.log(seen);
-          console.log(data.body);
-          throw new Error('duplicate contract code');
-        } else {
-          seen.push(data.body.CODIGO_CONTRATO);
+
+        const ci = seenContractCodes.indexOf(data.body.CODIGO_CONTRATO);
+        const hi = seenHashes.indexOf(data.hash);
+        const isIdenticalRow = (ci === hi);
+        if (ci > -1) {
+          console.log(`we've seen this code before`)
+          console.log(seenContractCodes[ci]);
+          console.log(seenHashes[hi]);
+          console.log(data.hash);
+          if (!isIdenticalRow) {
+            throw new Error('duplicate contract code');
+          }
         }
 
         const doc = valueMap(data.body);
@@ -113,26 +85,32 @@ function web2es(mapping, url) {
           _id: data.body.CODIGO_CONTRATO,
           hash: data.hash,
         });
+
+        seenContractCodes[countAll] = data.body.CODIGO_CONTRATO;
+        // seenContractCodes.push(data.body.CODIGO_CONTRATO);
+        seenHashes[countAll] = data.hash;
+        // seenHashes.push(data.hash);
       }
     }))
     .on('error', (error) => {
       throw error
     })
     .pipe(etl.collect(100))
-    .pipe(etl.elastic.index(client, INDEX, 'compranet', {
-        pushResults: true,
-    }))
-    .pipe(process.stdout)
+    .pipe(cmd)
+    .on('error', (error) => {
+      throw error
+    })
+    .promise()
+    .then((results) => {
+      console.log(results);
+      console.log(`indexed ${countIndexed} documents of ${countAll}`);
+    }, e => console.log('error',e));
+    // .pipe(process.stdout)
 }
 
-ping.then(() => {
-  console.log('Contacted elasticsearch');
-  createIndex().then((mapping) => {
-    let properties = {};
-    SOURCES.forEach((url) => {
-      web2es(properties, url);
-    });
-  })
-}, (error) => {
-  console.trace(error.message);
-});
+//createIndex().then((mapping) => {
+//  let properties = {};
+  SOURCES.forEach((url) => {
+    web2es(url)// ;
+  });
+// })
