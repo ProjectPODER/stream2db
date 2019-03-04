a358218406b068df4eab89d52405762fdf8d5880
Fernando Matzdorf <fernando@rindecuentas.org>
2019-03-04 17:09:41 -0600

    Aceptar stream de objetos JSON separados por newline desde stdin.

--- bin/app.js
+++ bin/app.js
@@ -2,7 +2,9 @@
 /* eslint no-console: ["error", { allow: ["time", "timeEnd"] }] */
 const args = require('../lib/args');
 const getSources = require('../lib/sources');
+const getStdin = require('../lib/stdin');
 const { ready, db } = require('../lib/db');
+const JSONStream = require('JSONStream');
 
 // FIXME first try internet and error out if no connection
 
@@ -31,4 +33,28 @@ if (args.uris) {
   }).catch(error => {
     throw error;
   });
+}
+else {
+    console.log('tratando de procesar stdin');
+    const mainStream = require('../lib/parse');
+    const dataStream = getStdin();
+
+    ready.then(() => {
+        const { backend, db: dbString, type } = args;
+        const infoString = `contacted ${backend}, ${dbString}, ${type}\n`;
+
+        process.stdout.write(infoString);
+        console.time('duration');
+        mainStream(dataStream)
+          .promise()
+          .then(() => {
+            db.close();
+            process.stdout.write(' ');
+            console.timeEnd('duration');
+        }, e => { process.stdout.write('error in stdin '); console.log(e); });
+        dataStream.resume();
+
+    }).catch(error => {
+        throw error;
+    });
 }
