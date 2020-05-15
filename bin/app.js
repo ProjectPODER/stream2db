10547760e0c8bd572f8814830f374ac4e41da247
Fernando Matzdorf <fernando@rindecuentas.org>
2020-05-15 11:11:59 -0500

    Updates: - Modified script output, only update summaries. - Useless file cleanup. - Removed count query to improve performance.

--- bin/app.js
+++ bin/app.js
@@ -6,8 +6,10 @@ const getStdin = require('../lib/stdin');
 const { ready, db } = require('../lib/db');
 const JSONStream = require('JSONStream');
 
-// FIXME first try internet and error out if no connection
+global.skippedRecords = 0;
+global.processedRecords = 0;
 
+// FIXME first try internet and error out if no connection
 if (args.uris) {
   const mainStream = require('../lib/parse');
   const sources = getSources(args.uris);
@@ -41,7 +43,7 @@ else {
 
     ready.then(() => {
         const { backend, db: dbString, type } = args;
-        const infoString = `contacted ${backend}, ${dbString}, ${type}\n`;
+        const infoString = `contacted: ${backend}, ${dbString}, ${type}\n`;
 
         process.stdout.write(infoString);
         console.time('duration');
@@ -49,7 +51,9 @@ else {
           .promise()
           .then(() => {
             db.close();
-            process.stdout.write(' ');
+            // Log important information
+            console.log('skipped:', skippedRecords);
+            console.log('processed:', processedRecords);
             console.timeEnd('duration');
         }, e => { process.stdout.write('error in stdin '); console.log(e); process.exit(1); });
         dataStream.resume();
